dd338faf06f884bee84a884f30eb7b3e637035ef
Avin <avinkavish@gmail.com>
2023-12-07 15:56:59 +0530

    Add sample content to fare and fare caps pages

--- next-typescript-app/src/app/fare-caps/page.tsx
+++ next-typescript-app/src/app/fare-caps/page.tsx
@@ -1,3 +1,33 @@
+"use client"
+import { Button } from "@/components/ui/button"
+import { DataTable } from "@/components/ui/data-table"
+
+const columns = [
+  { header: "From", accessorKey: "from" },
+  { header: "To", accessorKey: "to" },
+  { header: "Daily Cap", accessorKey: "dailyCap" },
+  { header: "Weekly Fare", accessorKey: "weeklyCap" },
+]
+
+const sampleData = [
+  {
+    id: "1",
+    from: "Green",
+    to: "Red",
+    dailyCap: 20,
+    weeklyCap: 60,
+  },
+]
+
 export default function FareCapsPage() {
-  return <div>FareCapsPage</div>
+  return (
+    <div className="mt-4 p-4 mx-auto max-w-screen-lg">
+      <div className="pb-2 flex justify-end">
+        <Button>Add Fare Caps</Button>
+      </div>
+      <div>
+        <DataTable columns={columns} data={sampleData} />
+      </div>
+    </div>
+  )
 }
--- next-typescript-app/src/app/fares/page.tsx
+++ next-typescript-app/src/app/fares/page.tsx
@@ -1,3 +1,33 @@
+"use client"
+import { Button } from "@/components/ui/button"
+import { DataTable } from "@/components/ui/data-table"
+
+const columns = [
+  { header: "From", accessorKey: "from" },
+  { header: "To", accessorKey: "to" },
+  { header: "Peak Fare", accessorKey: "peakFare" },
+  { header: "Off Peak Fare", accessorKey: "offPeakFare" },
+]
+
+const sampleData = [
+  {
+    id: "1",
+    from: "Green",
+    to: "Red",
+    peakFare: 8.5,
+    offPeakFare: 6,
+  },
+]
+
 export default function FaresPage() {
-  return <div>FaresPage</div>
+  return (
+    <div className="mt-4 p-4 mx-auto max-w-screen-lg">
+      <div className="pb-2 flex justify-end">
+        <Button>Add Fare</Button>
+      </div>
+      <div>
+        <DataTable columns={columns} data={sampleData} />
+      </div>
+    </div>
+  )
 }
