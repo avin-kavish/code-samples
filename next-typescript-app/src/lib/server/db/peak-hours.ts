138a9fd6e4edc0d8672cbf92732fa026ac37e7a6
Avin <avinkavish@gmail.com>
2023-12-13 14:53:53 +0530

    Return peak hours as object

--- next-typescript-app/src/lib/server/db/peak-hours.ts
+++ next-typescript-app/src/lib/server/db/peak-hours.ts
@@ -1,5 +1,11 @@
+import { DayOfWeek } from "@prisma/client"
 import { prisma } from "@/lib/server/db/client"
 
-export async function getPeakHours() {
+type PeakHours = Record<DayOfWeek, { start: string; end: string }>
+
+export async function getPeakHours(): Promise<PeakHours> {
   const hrs = await prisma.peakHour.findMany()
+  return Object.fromEntries(
+    hrs.map(hr => [hr.day, { end: hr.end, start: hr.start }]),
+  ) as PeakHours
 }
