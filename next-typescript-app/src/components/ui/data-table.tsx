2ceb75949a158e795b1b81b7c15d85cf0a6db241
Avin <avinkavish@gmail.com>
2023-12-23 12:54:56 +0530

    Add delete action to data table

--- next-typescript-app/src/components/ui/data-table.tsx
+++ next-typescript-app/src/components/ui/data-table.tsx
@@ -12,16 +12,94 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
+import { useMemo } from "react"
+import { Button } from "@/components/ui/button"
+import { IDType } from "@/lib/api/rest-client"
+import { TrashIcon } from "lucide-react"
+
+interface ActionProps {
+  id: IDType
+  delete?: (id: any) => Promise<void>
+}
+
+function Actions({ id, ...props }: ActionProps) {
+  return (
+    <div className="flex items-center gap-4">
+      {props.delete && (
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <Button size="icon" variant="ghost">
+              <TrashIcon className="h-4 w-4 text-muted-foreground" />
+            </Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete the
+                record.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction
+                variant="destructive"
+                onPointerDown={async () => {
+                  await props.delete!(id)
+                }}
+              >
+                Confirm
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      )}
+    </div>
+  )
+}
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
   data: TData[]
+  delete?: (id: any) => Promise<void>
 }
 
 export function DataTable<TData, TValue>({
-  columns,
+  columns: columnsProp,
   data,
+  ...rest
 }: DataTableProps<TData, TValue>) {
+  const columns = useMemo(() => {
+    const newColumns = [...columnsProp]
+
+    if (rest.delete) {
+      const actions = {
+        id: "actions",
+        header: "Actions",
+        cell: props => {
+          const id = (props.row.original as { id: IDType }).id
+          return <Actions id={id} delete={rest.delete} />
+        },
+      } satisfies ColumnDef<TData>
+
+      newColumns.push(actions)
+    }
+
+    return newColumns
+  }, [columnsProp])
+
   const table = useReactTable({
     data,
     columns,
