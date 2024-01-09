07625d678e4afbefcb03c8f6d176cac34e3cdaa8
Avin <avinkavish@gmail.com>
2024-01-09 21:27:30 +0530

    Add validation plugin to grapqhl

--- next-typescript-app/src/lib/server/graphql/schema-builder.ts
+++ next-typescript-app/src/lib/server/graphql/schema-builder.ts
@@ -1,5 +1,8 @@
 import SchemaBuilder from "@pothos/core"
+import ValidationPlugin from "@pothos/plugin-validation"
 
-export const builder = new SchemaBuilder({})
+export const builder = new SchemaBuilder({
+  plugins: [ValidationPlugin],
+})
 
 builder.queryType({})
