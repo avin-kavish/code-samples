8e3eb94b9899fa63673c05487de2c21265a8a0dc
Avin <avinkavish@gmail.com>
2024-06-09 18:38:08 +0530

    add users app

--- django-backend/articles/models.py
+++ django-backend/articles/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from users.models import User
 
 
 class Article(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=1000)
+    slug = models.CharField(max_length=1000)
     content = models.TextField()
     is_draft = models.BooleanField(default=True)
     tags = models.TextField(array=True)
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
--- django-backend/comments/models.py
+++ django-backend/comments/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
+
 class Comment(models.Model):
     text = models.TextField()
+
+    article = models.ForeignKey('articles.Article', on_delete=models.CASCADE)
+
+    parent_comment = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
+
+    creator = models.ForeignKey('users.User', on_delete=models.CASCADE)
 
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
